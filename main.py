9c2cd6f0916e8f41466b80c2dc345630fc887544
Shinobi <86633847+Shinobi7k@users.noreply.github.com>
2022-12-31 23:41:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -891,7 +891,7 @@ def DownlaodWindow():
                 # Convert if there is a conversion
                 if advanced_checker == "yes":
                     downloading_var.set("Converting")
-                    Conversion(final_name, ext, url.length)
+                    Conversion(final_name, "mp4", url.length)
                 # Finished
                 newWindow.bell()
                 downloading_var.set("Finished")
@@ -1576,7 +1576,7 @@ def PlaylistWindow():
                     # Convert if there is a conversion
                     if advanced_checker == "yes":
                         downloading_var.set("Converting")
-                        Conversion(final_name, ext, url.length)
+                        Conversion(final_name, "mp4", url.length)
 
         # If playlist is 720p, 360p, *audio
         else:
